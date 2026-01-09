c8ba96e6ba75a0fc0190b8182acaec2b19c5a8fc
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2026-01-09 11:28:44 -0600

    Fix ESLint TS rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,20 @@
+// module.exports = {
+//   extends: ['next/core-web-vitals'],
+//   rules: {
+//     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+//     '@typescript-eslint/no-explicit-any': 'warn',
+//   },
+// }
+
+
+
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
   rules: {
+    'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-explicit-any': 'warn',
   },
